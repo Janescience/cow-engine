6c5ffa5cbdf8d0ffe5dfeca76e6ea03c804594ad
janwit.si <janwit.si@ocean.co.th>
2022-09-13 14:46:20 +0700

    fix dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require("cors")
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -15,7 +16,6 @@ app.use(express.urlencoded({ extended:true }))
 app.use(cookieParser())
 
 const db = require("./src/models");
-dotenv.config();
 
 // routes
 require('./src/routes/auth.routes')(app);
