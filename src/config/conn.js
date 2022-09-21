de13336ab48594ff797758c3a31849d2ef38690e
janwit.si <janwit.si@ocean.co.th>
2022-09-21 14:03:43 +0700

    add env

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://janescience:Top2233223233@cluster0.xf552.mongodb.net/mern?retryWrites=true&w=majority', 
+mongoose.connect(process.env.URI, 
     { useNewUrlParser: true,
      useUnifiedTopology: true })
     .then((data) => {
