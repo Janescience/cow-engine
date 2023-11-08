c06570d2f8219919da7abb8f60675a57055d0c83
janwit.si <janescience.top@gmail.com>
2023-11-08 23:14:47 +0700

    change config db

--- src/config/conn.js
+++ src/config/conn.js
@@ -7,7 +7,7 @@ mongoose.connect(process.env.URI,
 })
 
 const db = require("../models");
-db.sequelize.sync({force:true})
+db.sequelize.sync()
   .then(() => {
     console.log("Synced db.");
   })
--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,11 +4,11 @@ dotenv.config();
 
 module.exports = {
   // HOST: "13.250.122.106",
-  HOST: process.env.HOST,
+  HOST: "dairyfarm-db.cj6bvhybetwa.ap-southeast-1.rds.amazonaws.com",
   USER: "postgres",
   PASSWORD: "postgres",
   // PASSWORD: "post123",
-  DB: process.env.DB,
+  DB: "postgres",
   dialect: "postgres",
   pool: {
     max: 20,
